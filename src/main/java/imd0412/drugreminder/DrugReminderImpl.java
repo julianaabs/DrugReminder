2176ef6f7e6d20fdbec83aec913b7035bffd2349
julianaabs <jubsbarbosa0@gmail.com>
2018-09-20 16:09:30 -0300

    first commit

---
+++ src/main/java/imd0412/drugreminder/DrugReminderImpl.java
@@ -0,0 +1,13 @@
+package imd0412.drugreminder;
+
+import java.util.List;
+
+public class DrugReminderImpl implements IReminder {
+
+	public List<String> createReminders(String startTime, Frequency frequency,
+			Integer duration) {
+		throw new IllegalStateException(
+				"Method createReminders not yet implemented.");
+	}
+
+}
